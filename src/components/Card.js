d5ac69efc26bb24f64457f54905dcf3ef69d1685
Suminporn <minmin1527ryo@gmail.com>
2025-04-29 23:21:33 +0700

    fix card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 export default function Card({name, mediatype, imageUrl}) {
     return (
-        <div className="bg-[#353A67] shadow-md flex items-center rounded-[20px] p-5 my-5 gap-10">
+        <div className="bg-[#353A67] shadow-md flex items-center justify-between rounded-[20px] p-5 my-5 gap-10">
             <div className="flex-col">
             <h2 className="text-[30px] text-white font-bold pt-[5px]">{name}</h2>
             <p className="text-[16px] text-white opacity-70 pt-[8px] pb-[5px]">{mediatype}</p>
