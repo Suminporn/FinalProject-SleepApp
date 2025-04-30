2bf04ab878b06bb2369844372830d5a14cd9ab17
Suminporn <minmin1527ryo@gmail.com>
2025-04-30 18:48:48 +0700

    fix error

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -9,7 +9,7 @@ export default function Explore() {
         <main>
           <h1 className="text-[30px] font-bold text-white">Collections</h1>
           <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">New and Popular</h2>
-          <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">Woman's Collection</h2>
+          <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">Woman Collection</h2>
           <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">Calming Everyday Anxiety</h2>
           <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">Binaural Beats</h2>
           <h2 className="text-black bg-orange-200 shadow-md rounded-[20px] flex p-5 mb-3">Reframe Stress and Relax</h2>
